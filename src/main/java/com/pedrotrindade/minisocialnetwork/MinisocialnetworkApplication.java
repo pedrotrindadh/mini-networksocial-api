a3679c4ee9ea64be37e0f11b19bd98c252a1d26b
Pedro Trindade <prindade@outlook.com>
2026-04-17 12:13:48 -0300

    Project created

---
+++ src/main/java/com/pedrotrindade/minisocialnetwork/MinisocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.pedrotrindade.minisocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinisocialnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrotrindade/minisocialnetwork/MinisocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrotrindade.minisocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
