cad7cc8bca72e44023f17b4ea510191da8530534
Pedro Trindade <prindade@outlook.com>
2026-04-28 10:58:43 -0300

    Database instantiation operation

---
+++ src/main/java/com/pedrotrindade/minisocialnetwork/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.pedrotrindade.minisocialnetwork.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.pedrotrindade.minisocialnetwork.entities.User;
+import com.pedrotrindade.minisocialnetwork.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Green", "marria@gmail.com");
+		User alex = new User(null, "Alex Brown", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
