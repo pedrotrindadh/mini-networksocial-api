26d845b2e97bb477ed616696bdec843ef6af84da
Pedro Trindade <prindade@outlook.com>
2026-04-29 12:36:15 -0300

    Delete User

--- src/main/java/com/pedrotrindade/minisocialnetwork/services/UserService.java
+++ src/main/java/com/pedrotrindade/minisocialnetwork/services/UserService.java
@@ -29,6 +29,10 @@ public class UserService {
 		return userRepository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		userRepository.delete(findById(id));
+	}
+	
 	public User fromDTO (UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
