b3f27bbb9a4ac3db67e5e4c1ae0318683e556113
Pedro Trindade <prindade@outlook.com>
2026-05-04 14:55:17 -0300

    Endpoint to return the posts

--- src/main/java/com/pedrotrindade/minisocialnetwork/resources/UserResource.java
+++ src/main/java/com/pedrotrindade/minisocialnetwork/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.pedrotrindade.minisocialnetwork.domain.Post;
 import com.pedrotrindade.minisocialnetwork.domain.User;
 import com.pedrotrindade.minisocialnetwork.dto.UserDTO;
 import com.pedrotrindade.minisocialnetwork.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
